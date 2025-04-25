e56ca89438b92ca1cd1445c95325bd6bdd3f25c5
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-04-25 10:20:51 +0545

    feat: make navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,65 @@
+<template>
+  <header class="header">
+    <div class="header__icon">
+      <div class="circle"></div>
+      <h2>Reddit</h2>
+    </div>
+    <div class="header__searchbar">
+      <input type="text" placeholder="Search on Reddit" v-model="searchString" />
+    </div>
+    <div class="header__links">
+      <RouterLink v-for="{ link, text } in menuItems" :key="link" :to="link">{{ text }}</RouterLink>
+      <span>{{ searchString }}</span>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+const searchString = defineModel('')
+
+const menuItems = [
+  { text: 'All', link: '/r/all' },
+  { text: 'Popular', link: '/r/popular' },
+]
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.25rem 1rem;
+  border-bottom: 1px solid hsl(195, 3%, 25%);
+
+  &__icon {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+
+    .circle {
+      width: 2rem;
+      height: 2rem;
+      background: hsl(16, 100%, 50%);
+      border-radius: 50%;
+    }
+  }
+
+  &__searchbar {
+    width: 100%;
+    max-width: 34.375rem;
+    padding-block: 0.125rem;
+
+    input {
+      padding: 0.5rem 1rem;
+      border-radius: 100vw;
+      background: hsl(200, 12%, 19%);
+      width: 100%;
+    }
+  }
+
+  &__links {
+    display: flex;
+    gap: 1rem;
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/sass";`,
+        additionalData: `@use "@/assets/sass";`,
       },
     },
   },
