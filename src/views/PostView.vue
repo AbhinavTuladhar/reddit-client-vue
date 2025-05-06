430ba429278e7b427b59603a74b1c7286828d3dc
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-05-06 15:50:58 +0545

    feat: make api request in single post page

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <span> {{ id }} - {{ subreddit }} - {{ title }}</span>
+    <span> {{ params.id }} - {{ params.subreddit }} - {{ params.title }}</span>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else-if="isError">Error</div>
+    <div v-else-if="!data">No data</div>
+    <div v-else>
+      <pre> {{ JSON.stringify(data, null, 2) }} </pre>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
 import { useRoute } from 'vue-router'
+import PostService from '@/services/post.service'
+import { computed, watch } from 'vue'
 
 interface PostParams {
   subreddit: string
@@ -14,7 +23,24 @@ interface PostParams {
 }
 
 const router = useRoute()
-const { id, subreddit, title } = router.params as unknown as PostParams
+const params = computed(() => router.params as unknown as PostParams)
+
+const { data, isLoading, isError } = useQuery({
+  queryKey: ['post', params.value.id],
+  queryFn: () => PostService.getPost(params.value.id),
+})
+
+watch(data, () => {
+  if (!data.value) {
+    return
+  }
+  console.log(data.value)
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+pre {
+  max-width: 100%;
+  white-space: pre-wrap;
+}
+</style>
