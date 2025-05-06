b7e70c2e35caa5a22e89e8320540a57c2f60ee16
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-05-06 15:06:04 +0545

    feat: make simple page for post detail, make comment count dynamic

---
+++ src/views/PostView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span> {{ id }} - {{ subreddit }} - {{ title }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+interface PostParams {
+  subreddit: string
+  id: string
+  title: string
+}
+
+const router = useRoute()
+const { id, subreddit, title } = router.params as unknown as PostParams
+</script>
+
+<style scoped></style>
