afe3ac7408630ef8f1a7fad798b76e11d7ddf882
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-06-15 15:19:49 +0545

    feat: add total and active members info

--- src/components/subreddit/SubredditSidebar.vue
+++ src/components/subreddit/SubredditSidebar.vue
@@ -11,11 +11,27 @@
         </div>
         <div class="sidebar__icon-text-row">
           <Cake />
-          <span> {{ information.creationDate }} </span>
+          <span> Created on {{ information.creationDate }} </span>
         </div>
-        <div class="sidebar__icon-text-row">
-          <Cake />
-          <span> {{ information.subscribers.toLocaleString() }} </span>
+        <div class="sidebar__members-grid">
+          <div>
+            <div class="sidebar__member-count">
+              {{ information.subscribers }}
+            </div>
+
+            <div class="sidebar__member-text">Members</div>
+          </div>
+
+          <div>
+            <div class="sidebar__member-count">
+              {{ information.online }}
+            </div>
+
+            <div class="sidebar__online-row">
+              <div class="sidebar__green-circle"></div>
+              <div class="sidebar__member-text">Online</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -42,7 +58,8 @@ const { data, isLoading, isError } = useQuery({
 const information = computed(() => {
   if (!data.value) return
 
-  const { title, public_description, created_utc, subscribers } = data.value || {}
+  const { title, public_description, created_utc, subscribers, active_user_count } =
+    data.value || {}
 
   const creationDate = new Date(created_utc * 1000).toLocaleDateString('fr-CA')
 
@@ -51,6 +68,7 @@ const information = computed(() => {
     description: public_description,
     creationDate,
     subscribers: numberFormatter.format(subscribers),
+    online: numberFormatter.format(active_user_count),
   }
 })
 </script>
@@ -64,17 +82,47 @@ const information = computed(() => {
   background: black;
   overflow: auto;
   color: var(--reddit-weak-text);
+  font-size: 0.875rem;
 
   &__title {
-    font-size: 1.125rem;
     font-weight: bold;
     color: white;
+    font-size: inherit;
   }
 
   &__icon-text-row {
     display: flex;
     gap: 0.5rem;
     align-items: center;
+    margin-top: 0.25rem;
+  }
+
+  &__members-grid {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    margin-top: 0.75rem;
+  }
+
+  &__member-count {
+    color: white;
+    font-weight: bold;
+  }
+
+  &__member-text {
+    font-size: 0.875rem;
+  }
+
+  &__online-row {
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+  }
+
+  &__green-circle {
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 100%;
+    background: green;
   }
 }
 </style>
